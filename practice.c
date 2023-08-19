1bb4797b53859564b2a36baf24b8f0d24c59c298
Simriti <simritib1@gmail.com>
2023-08-20 04:32:43 +0800

    Update practice.c

--- practice.c
+++ practice.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-int main() {
+int declareVariable() {
   
   // Fix the missing data types
   int studentRank;
@@ -15,3 +15,27 @@ int main() {
  printf("Student's Total Fees: $%3.2f\n", studentFeeTotal);
  printf("Student's Grade: %c\n", studentGradeLetter);
 }
+
+#include <stdio.h>
+
+int variableInPrint() {
+  
+ int ageLearnedToRide = 5;
+
+ printf("I was %d years old when I learned to ride a bike.\n", ageLearnedToRide);
+ printf("I hope I still remember how to ride.");
+}
+
+int initialiseVariable() {
+  
+  int numOfBooks = 2;
+  char favLetter = 'r';
+  char favDigit = '9';
+  double costOfCandyBar = 0;
+
+  printf("Number of books: %d\n", numOfBooks);
+  printf("Your Favorite Letter is: %c\n", favLetter);
+  printf("Your Favorite Digit is: %c\n", favDigit);
+  printf("You expect to pay $%.2f for a candy bar.\n", costOfCandyBar);
+}
+
