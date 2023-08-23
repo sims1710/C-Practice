261a14ded83b5974912e109f9050393b927113e9
Simriti <simritib1@gmail.com>
2023-08-24 01:58:46 +0800

    Update practice.c

--- practice.c
+++ practice.c
@@ -54,3 +54,14 @@ int updatingVariables() {
   printf("The book version has a review score of %c and costs $%.2f\n", bookVersionReview, bookPrice);
   printf("The movie version has a review score of %c and costs $%.2f\n", movieVersionReview, ticketPrice);
 }
+
+int constant() {
+  
+// Speed of light is 1.86e5 mi/s or 186000 mi/s we will store 1.86 and do the multiplicaiton later.
+ const double SPEEDOFLIGHT = 1.86;
+  int timeTraveledInSeconds = 30;
+
+  // No need to change below here
+  printf("Light would travel %.2f miles in %d seconds\n", SPEEDOFLIGHT * 100000 * timeTraveledInSeconds, timeTraveledInSeconds);
+
+}
