e663876fd96e4d7659293d40a210f1ea7ae7b4a1
Simriti <simritib1@gmail.com>
2023-08-24 02:01:16 +0800

    Update practice.c

--- practice.c
+++ practice.c
@@ -65,3 +65,15 @@ int constant() {
   printf("Light would travel %.2f miles in %d seconds\n", SPEEDOFLIGHT * 100000 * timeTraveledInSeconds, timeTraveledInSeconds);
 
 }
+
+int castingTypes() {
+  
+  double testScore = 95.7;
+  int displayScore = 0;
+
+  displayScore = (int)testScore;
+
+  // No need to change below here
+  printf("Great work, you got a %d%% on your test\n", displayScore);
+
+}
