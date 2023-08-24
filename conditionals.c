58dc33f79ce85d72a621eebc7302d1b9b55b9a4f
Simriti <simritib1@gmail.com>
2023-08-25 02:54:24 +0800

    Update conditionals.c

--- conditionals.c
+++ conditionals.c
@@ -152,3 +152,76 @@ int space() {
     break;
   }
   printf("\nYour weight: %lf\n", weight);
+
+int printingTheYear(void) {
+  int month, day, year;
+
+  // Standard date form
+  printf("Enter date (mm/dd/yyyy): ");
+  // Split the user input into 3 variables for the date
+  // Don't worry about the `scanf()` below, you'll learn more about these later!
+  scanf("%d /%d /%d", &month, &day, &year);
+
+  // Print the month
+
+  switch (month) {
+    case 1:
+    printf("January");
+    break;
+    case 2:
+    printf("February");
+    break;
+    case 3:
+    printf("March");
+    break;
+    case 4:
+    printf("April");
+    break;
+    case 5:
+    printf("May");
+    break;
+    case 6:
+    printf("June");
+    break;
+    case 7:
+    printf("July");
+    break;
+    case 8:
+    printf("August");
+    break;
+    case 9:
+    printf("September");
+    break;
+    case 10:
+    printf("October");
+    break;
+    case 11:
+    printf("November");
+    break;
+    case 12:
+    printf("December");
+    break;
+  }
+  
+  // Print the day
+  // Print the suffix for a given day
+  switch (day) {
+    case 1: case 21: case 31:
+        printf(" %dst", day);
+        break;
+    case 2: case 22:
+        printf(" %dnd", day);
+        break;
+    case 3: case 23:
+        printf(" %drd", day);
+        break;
+    default:
+        printf(" %dth", day);
+        break;
+}
+
+  // Print the year
+  printf(" %d", year);
+  
+  return 0;
+}
