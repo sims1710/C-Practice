b50d369e50094e1c770692121b4678b0f959411a
Simriti <simritib1@gmail.com>
2023-08-25 02:18:55 +0800

    Update conditionals.c

--- conditionals.c
+++ conditionals.c
@@ -1,1 +1,13 @@
+int coinFlip() {
+  // Create a number that’s 0 or 1
+  srand (time(NULL));
+  int coin = rand() % 2;
 
+  // If number is 0: Heads
+  // If it is not 0: Tails
+  if (coin == 0) {
+    printf("Heads\n");
+  } else {
+    printf("Tails\n");
+  }
+}
