a63f292423520845304a4c0b3df0de38697a7507
Simriti <simritib1@gmail.com>
2023-08-25 02:21:27 +0800

    Update conditionals.c

--- conditionals.c
+++ conditionals.c
@@ -11,3 +11,18 @@ int coinFlip() {
     printf("Tails\n");
   }
 }
+
+int ifStatement() {
+
+  int grade1 = 90;
+  int grade2 = 59;
+
+  if (grade1 > 60){
+    printf("Pass\n");
+  }
+
+  if (grade2 < 60){
+    printf("Fail\n");
+  }
+
+}
