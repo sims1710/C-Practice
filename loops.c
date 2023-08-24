fa6ea7fbf977c806a92f219c5a1266da00fb7725
Simriti <simritib1@gmail.com>
2023-08-25 03:06:13 +0800

    Update loops.c

--- loops.c
+++ loops.c
@@ -1,1 +1,13 @@
+int whileLoop() {
 
+  int i = 9;
+  int square = 0;
+
+  // Write a while loop here:
+  while (i >= 0){
+    square = i * i;
+    printf("%d\t%d\n", i, square);
+    i --;
+  }
+
+}
