38370ebf6367cfc6d2463ec1773ca185812d14a8
Simriti <simritib1@gmail.com>
2023-08-25 02:28:45 +0800

    Update conditionals.c

--- conditionals.c
+++ conditionals.c
@@ -60,3 +60,17 @@ int elseClause() {
   }
 
 }
+
+int elseIf() {
+ 
+  double ph = 7;
+
+  // Start the if, else if, else here:
+  if (ph > 7) {
+    printf("Basic\n");
+  } else if (ph < 7) {
+    printf("Acidic\n");
+  } else {
+    printf("Neutral");
+  }
+}
