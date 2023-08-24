3beb314607b86e173d289f869b792ba07b583db4
Simriti <simritib1@gmail.com>
2023-08-25 02:25:23 +0800

    Update conditionals.c

--- conditionals.c
+++ conditionals.c
@@ -41,3 +41,22 @@ int workingWithOperators() {
   }
 
 }
+
+int elseClause() {
+
+  int grade1 = 59;
+  int grade2 = 90;
+
+  if (grade1 > 60) {
+    printf("Pass\n");
+  } else {
+    printf("Fail\n");
+  }
+
+  if (grade2 > 60) {
+    printf("Pass\n");
+  } else {
+    printf("Fail\n");
+  }
+
+}
