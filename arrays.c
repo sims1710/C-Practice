dce5d37f27aad1f9bdc38ed870315060827c487d
Simriti <simritib1@gmail.com>
2023-08-26 13:40:06 +0800

    Update arrays.c

--- arrays.c
+++ arrays.c
@@ -17,3 +17,13 @@ int loopingThroughArrays() {
     arr2[i] = i + 4;
   }
 }
+
+int usingSizeOf() {
+  double arr[] = {1.0, 8, 2.27, 16.2, 2, 3.14, 12.6, 1.62, 6.54, 9, 11};
+  int len = sizeof(arr)/sizeof(double);
+  printf("%i\n", len);
+  // Checkpoint 2 code goes here.
+  for(int i = 0; i < len; i++){
+    printf("%f\n", arr[i]);
+  }
+}
