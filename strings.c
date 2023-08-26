6614c8bc044584f1f0d5b160041925042cf4ccbf
Simriti <simritib1@gmail.com>
2023-08-26 18:11:25 +0800

    Update strings.c

--- strings.c
+++ strings.c
@@ -1,10 +1,51 @@
-int initialisingAndCreatingStrings() {
-  // Checkpoint 1 code goes here
-  char comp[] = {'C', 'o', 'm', 'p', 'u', 't', 'e', 'r', '\0'};
-  
-  // Checkpoint 2 code goes here
-  char compCode[] = {"Computer Code"};
+#include<stdio.h>
 
-  // Checkpoint 3 code goes here      
+int initialisingAndCreatingStrings() {
+  char comp[] = {'C', 'o', 'm', 'p', 'u', 't', 'e', 'r', '\0'};
+  char compCode[] = {"Computer Code"};  
   printf("%s\n%s", comp, compCode);
+}
+
+int characterAccessAndModification() {
+  char p[] = "procezzor";
+  // Checkpoint 1 code goes here
+  printf("%c\n", p[2]);
+  // Checkpoint 2 code goes here
+  p[5] = 's';
+  p[6] = 's';
+  printf("%s", p);
+}
+
+#include<string.h>
+
+int loopingThroughString() {
+  char s[] = "When the zombies arrive, quickly fax Judge Pat Alphabet";
+  char p[] = "poolloop";  
+  // Checkpoint 1 code goes here
+  for(int i = 0; i < strlen(s); i++) {
+    s[i] = '*';
+  }
+  printf("%s\n", s);
+  // Checkpoint 2 code goes here
+  for(int i = 0; i < strlen(p)/2; i++) {
+    p[i] = '#';
+  }
+  printf("%s", p);
+}
+
+int stringConcatenation() {
+    
+  char s1[] = "London";
+  char s2[] = " Bridge";
+
+  char n[] = "New";
+  char y[] = " York";
+  char c[] = " City";
+
+  strcat(s1, s2);
+  printf("%s\n", s1); 
+
+  strcat(n, y);
+  strcat(n, c);
+  printf("%s", n); 
 }
