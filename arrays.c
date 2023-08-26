0ee93699a67a8563682b1a5a3502cc55fd6a0dd0
Simriti <simritib1@gmail.com>
2023-08-26 14:08:57 +0800

    Update arrays.c

--- arrays.c
+++ arrays.c
@@ -32,3 +32,21 @@ int multidimensionalArrays() {
   int arr [9][9];
   int matrix [3][3] = {{2, 9, 11}, {3, 1, 4}, {8, 3, 1}};
 }
+
+int elementAccessInMultidimensionalArrays() {
+  int matrix[][4] = {{14, 10, 6, 4}, {3, 7, 18, 11}, {13, 9, 5, 17}, {19, 12, 2, 1}}; 
+  int sum = 0;
+
+  // Checkpoint 1 code goes here.
+  printf("%i\n", matrix[3][1]);
+
+  // Checkpoint 2 code goes here.
+  int rowDimension = sizeof(matrix)/sizeof(matrix[0]);
+  int columnDimension = sizeof(matrix[0])/sizeof(int);
+
+  for(int i = 0; i < rowDimension; i++){
+    for(int j = 0; j < columnDimension; j++){
+      sum += matrix[i][j];
+  }
+}
+}
