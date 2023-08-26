dba4aad9a51fceab42987beeee9a7d39e676c757
Simriti <simritib1@gmail.com>
2023-08-26 14:16:00 +0800

    Update strings.c

--- strings.c
+++ strings.c
@@ -1,1 +1,10 @@
+int initialisingAndCreatingStrings() {
+  // Checkpoint 1 code goes here
+  char comp[] = {'C', 'o', 'm', 'p', 'u', 't', 'e', 'r', '\0'};
+  
+  // Checkpoint 2 code goes here
+  char compCode[] = {"Computer Code"};
 
+  // Checkpoint 3 code goes here      
+  printf("%s\n%s", comp, compCode);
+}
