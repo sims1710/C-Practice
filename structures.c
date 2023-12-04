a04ae99983e55edd2407f269ba4a7e9c5db5679d
Simriti <simritib1@gmail.com>
2023-12-04 10:11:37 +0800

    Update structures.c

--- structures.c
+++ structures.c
@@ -37,3 +37,23 @@ int main(void) {
   printf("Hello my name is %s and I am %d years old.\n", person2.name, person2.age);
 }
 
+// STRUCTURE POINTERS
+#include <stdio.h>
+struct Person {
+  char name[40];
+  int age;
+};
+
+int main(void) {
+  struct Person person1 = {"Martin", 57};
+  struct Person person2 = {"Michelle", 27};
+  // Write your code below
+  struct Person* person1Pointer = &person1;
+  struct Person* person2Pointer = &person2;
+
+  (*person1Pointer).age += 1; // dereferencing and dot notation
+  person2Pointer->age += 10; // arrow notation
+
+  printf("Hello my name is %s and I am %d years old.\n", person1.name, person1.age);
+  printf("Hello my name is %s and I am %d years old.\n", person2.name, person2.age);
+}
