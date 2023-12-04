29526c81944fae536059d60adf4be8734396fcd4
Simriti <simritib1@gmail.com>
2023-12-04 08:16:30 +0800

    Update functions.c

--- functions.c
+++ functions.c
@@ -48,3 +48,31 @@ int main(void) {
   incrementAge(&age);
   printf("%d\n", age);
 }
+
+###FUNCTION PROTOTYPES###
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+void repeatDigit(int);
+int getRandomNumber(int);
+
+// Define prototypes above
+// the function definitions
+void repeatDigit(int repetitions) {
+  int digit = getRandomNumber(9);
+  for(int i = 0; i < repetitions; i++) {
+    printf("%d ", digit);
+  }
+  printf("\n");
+}
+
+int getRandomNumber(int maxNumber) {
+  int randomNumber = rand() % maxNumber + 1;
+  return randomNumber;
+}
+
+int main(void) {
+  srand(time(NULL));
+  int repetitions = getRandomNumber(10);
+  repeatDigit(repetitions);
+}
